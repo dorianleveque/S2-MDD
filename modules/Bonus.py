95c297267d4cf55587a94632b0f5a10fe2cdf60b
dorianleveque <dorian.leveque@outlook.fr>
2016-04-07 12:01:40 +0200

    Added files via upload

---
+++ modules/Bonus.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+##################################################################
+##								##
+##	     The Legend Of Zelda - A Link to the Rogue		##
+##	    Un projet de Methode de Developpement (MDD) 	##
+##								##
+##			      Arrow.py				##
+##								##
+## LEVEQUE Dorian & ROUE Evan 	S2P 	ENIB	     01/04/2016 ##
+##################################################################
+
+def create():
+
+
+def getName(b):
+        return name
+        
+def setName(b, name):
+
+
+def getAmount(b):
+        return amount
+        
+def setAmount(b, amount):
+
+
---
+++ modules/chest.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+##################################################################
+##								##
+##	     The Legend Of Zelda - A Link to the Rogue		##
+##	    Un projet de Methode de Developpement (MDD) 	##
+##								##
+##			      Chest.py				##
+##								##
+## LEVEQUE Dorian & ROUE Evan 	S2P 	ENIB	     01/04/2016 ##
+##################################################################
+
+
+def create(x,y,donne): 
+        
+        
+def addItem(c,i): 
+
+
+def getPosition(c):
+        return (x,y)
+
+def setPosition(c,x,y):
+        
+
+def getContent(c):
+        return c["content"]
+        
+def isInside(c, Item):    #test du getContent
+        return true
