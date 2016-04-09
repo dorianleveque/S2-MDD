37745800e59268773a13d14cef4d1b2e8ff329c6
Evan Roué <roue.evan1@gmail.com>
2016-04-09 12:34:07 +0200

    Add Player module

---
+++ modules/Player.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+##################################################################
+##								##
+##	     The Legend Of Zelda - A Link to the Rogue		##
+##	    Un projet de Methode de Developpement (MDD) 	##
+##								##
+##			     Player.py	        		##
+##								##
+## LEVEQUE Dorian & ROUE Evan 	S2P 	ENIB	     09/04/2016 ##
+##################################################################
+
+# Modules personnalisés
+import Chest
