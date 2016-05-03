d705bb86c4917b33eaef6219461b48f552373c06
dleveque <levequedo@gmail.com>
2016-05-03 19:41:25 +0200

    Modification du module Utils apporté !
    - import sys non effectué auparavant 
    conclusion: fonction fonctionnelle

--- modules/Utils.py
+++ modules/Utils.py
@@ -9,8 +9,10 @@
 ## LEVEQUE Dorian & ROUE Evan   S2P     ENIB         22/04/2016 ##
 ##################################################################
 
-def goto(x, y):
-        s_x = str(int(x+1))
-        s_y = str(int(y+1))
+import sys
+
+def goto(x=0, y=0):
+        s_x = str(int(x))
+        s_y = str(int(y))
         txt = "\033[" + s_y + ";" + s_x + "H"
         sys.stdout.write(txt)
