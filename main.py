63277ba9ebaf021be14f4f94c89378994a03fc60
dorianleveque <dorian.leveque@outlook.fr>
2016-05-13 11:43:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -16,12 +16,15 @@ import time
 import select
 import tty 
 import termios
-sys.path.append('./modules/')
+
 
 # Modules personnalisés
-#import Game
+sys.path.append('./modules/')
+import Game
 import Utils
 import Player
+import Menu
+
 
 # Interaction clavier
 old_settings = termios.tcgetattr(sys.stdin)
@@ -29,11 +32,10 @@ old_settings = termios.tcgetattr(sys.stdin)
 # Donnee du jeu
 direction = "None"
 
+game = Game.create()
+menu = Menu.create()
 
-######### Test ###########
-player = Player .create()
-#########################
-
+refresh = True
 
 def init():
         #interaction clavier
@@ -41,81 +43,91 @@ def init():
         
         #cacher le curseur
         os.system('setterm -cursor off')
-        os.system('setterm -repeat off')
+        #os.system('setterm -repeat off')
+        
+
+        Menu.setCurrentWindow(menu,"mainMenu")
 
 
-def show():
+        
+#def show():
         #effacer la console 
-        sys.stdout.write("\033[1;1H")
-        sys.stdout.write("\033[2J")
+        #sys.stdout.write("\033[1;1H")
+        #sys.stdout.write("\033[2J")
         #os.system('setterm -clear all')
        
         
-        #affichage du joueur)
-        #deplacement du joueur:
-        x, y = Player.getPosition(player)
-        Utils.goto(x,y)
-        sys.stdout.write("Link")
+        ##affichage du joueur)
+        ##deplacement du joueur:
+        #x, y = Player.getPosition(player)
+        #Utils.goto(x,y)
+        #sys.stdout.write("Link")
+        
+        
 
-
-
-def run():     
-        cmp = 0
+def run():
         while True:
+                
+                currentWindowName = Menu.getCurrentWindowName(menu)
+                if currentWindowName != "Game":
+                        if refresh==True:
+                                Menu.show(menu)
+                else:
+                        Game.show(game)
                 interact()
-                move()
-                if cmp%10==0 : 
-                        show()
-                time.sleep(0.02)
-
-
-def move():
-        global player, direction      
+                #move()
+                time.sleep(0.2)
+        
+#def move():
+        #global player, direction      
        
-        #deplacement du joueur:
-        x, y = Player.getPosition(player)
-        dt = 0.25
+        ##deplacement du joueur:
+        #x, y = Player.getPosition(player)
+        #dt = 0.25
 
-        if direction == 'Up': Player.setPosition(player, x, y-1*dt)
-        if direction == 'Down': Player.setPosition(player, x, y+1*dt)
-        if direction == 'Right': Player.setPosition(player, x+2*dt, y)
-        if direction == 'Left': Player.setPosition(player, x-2*dt, y)
-        direction = " "
+        #if direction == 'Up': Player.setPosition(player, x, y-1*dt)
+        #if direction == 'Down': Player.setPosition(player, x, y+1*dt)
+        #if direction == 'Right': Player.setPosition(player, x+2*dt, y)
+        #if direction == 'Left': Player.setPosition(player, x-2*dt, y)
+        #direction = " "
 
         
         #print Player.getPosition(player)    
         
 
 def interact(): 
-        global direction
+        global direction, refresh, game
        
+        refresh = False
         #gestion des evenements clavier
         if isData():                                    #si une touche est appuyee
                 read = sys.stdin.read(1)
+                
+                currentWindowName = Menu.getCurrentWindowName(menu)
+                
+                if currentWindowName != "Game":
+                        Menu.interact(menu, read)
+                else:
+                        Game.interact(game, read)
+                        
                 if read == "\x1b": 
                         quit()                          # \x1b = touche echap / appel de la fonction qui permet de quitter le jeu
-                elif read == "z": 
-                        direction = "Up"                # le joueur se déplace vers Direction Haut
-                elif read == "q": 
-                        direction = "Left"              # le joueur se déplace vers Direction Gauche
-                elif read == "s": 
-                        direction = "Down"              # le joueur se déplace vers Direction Bas
-                elif read == "d": 
-                        direction = "Right"             # le joueur se déplace vers Direction Droite
-                #elif read == "p":                      # appel de la fonction pause
+
                 #elif read == "\x08": 
-        
+                refresh = True
+                
+                
+                
         
         
         while isData():
                 sys.stdin.read(5)
                 #termios.tcflush(sys.stdin, termios.TCIFLUSH)   # Permet de vider, le buffer des touches d'entree
-
+                
 
 def isData():
         #recuperation des elements clavier
         return select.select([sys.stdin], [], [], 0.0) == ([sys.stdin], [], [])
-
 
 def quit():
         global old_settings
@@ -126,7 +138,5 @@ def quit():
         os.system('setterm -cursor on')
         
         sys.exit()
-
-
 init()
 run()
