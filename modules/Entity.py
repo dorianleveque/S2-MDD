0711c6a948eb507105716f21a348a280892f2f4c
dorianleveque <dorian.leveque@outlook.fr>
2016-05-29 16:51:54 +0200

    Changement de vendredi et +
    Refonte du entity
    le module player se trouve maintenant dans le room
    les colisions et les déplacement ne fonctionne plus

--- modules/Entity.py
+++ modules/Entity.py
@@ -14,77 +14,109 @@
 # Modules personnalisés
 import Utils
 
-def create(x, y, health, xp, strength, resistance, sprite):
-        # creation d'une entité :
-        entity=dict()
-        entity["x"] = x
-        entity["y"] = y
-        entity["health"] = health
-        entity["xp"] = xp
-        entity["strength"] = strength
-        entity["resistance"] = resistance
-        entity["sprite"] = sprite
+#def create(entity, x, y, health, xp, strength, resistance, sprite):
+        ## creation d'une entité :
+        #entity["x"] = x
+        #entity["y"] = y
+        #entity["health"] = health
+        #entity["xp"] = xp
+        #entity["strength"] = strength
+        #entity["resistance"] = resistance
+        #entity["sprite"] = sprite
         
-        return entity
+        #return entity
 
-def getPosition(entity):
-        x = entity["x"]
-        y = entity["y"]
-        
+def getPosition(e):
+        x = e["x"]
+        y = e["y"]
         return (x,y)
 
-def setPosition(entity,x,y):
-        entity["x"] = x
-        entity["y"] = y
+def setPosition(e,x,y):
+        e["x"] = x
+        e["y"] = y
 
-def move(entity, direction):
-        if direction == "up":
-                entity["y"] += -1
-        if direction == "down":
-                entity["y"] += 1
-        if direction == "right":
-                entity["x"] += 1
-        if direction == "left":
-                entity["x"] += -1
+def getSpeed(e):
+        vx = e["vx"]
+        vy = e["vy"]
+        return (vx,vy)
+
+def setSpeed(e,vx,vy):
+        e["vx"] = vx
+        e["vy"] = vy
+
+
+def move(e, dt):
+        x,y = getPosition(e)
+        vx,vy = getSpeed(e)
+        
+        x = x + vx * dt
+        y = y + vy * dt
+        setPosition(e,x,y)
+        
+        
+        #if direction == "up":
+                #entity["y"] += -1
+        #if direction == "down":
+                #entity["y"] += 1
+        #if direction == "right":
+                #entity["x"] += 1
+        #if direction == "left":
+                #entity["x"] += -1
+
+#def collide():
+        #x, y = Player.getPosition(g["player"])
+        #currentRoom = Dungeon.getCurrentRoom(g["dungeon"])
+
+        #if (key == "z") and (Room.get(currentRoom, x, y-1) == " "): 
+                #y = y - 1             # le joueur se déplace vers Direction Haut
+        #elif (key == "q") and (Room.get(currentRoom, x-1, y) == " "): 
+                #x = x - 1             # le joueur se déplace vers Direction Gauche
+        #elif (key == "s") and (Room.get(currentRoom, x, y+1) == " "): 
+                #y = y + 1             # le joueur se déplace vers Direction Bas
+        #elif (key == "d") and (Room.get(currentRoom, x+1, y) == " "): 
+                #x = x + 1             # le joueur se déplace vers Direction Droite
 
 def show(e):
-        Utils.goto(e["x"]+2, e["y"]+2)
-        Utils.write(e["sprite"]+'\n')
+        x,y = getPosition(e)
+        sprite = getSprite(e)
+        Utils.goto(x+2, y+2)
+        Utils.write(sprite+'\n')
+        
+def getType(e):
+        return e["type"]
+        
+def setType(e, type):
+        e["type"] = type
 
 def getHealth(e):
         return e["health"]
 
 def setHealth(e, health):
         e["health"] = health
-        return
 
 def getXp(e):
         return e["xp"]
 
 def setXp(e, xp):
         e["xp"] = xp
-        return
 
 def getStrength(e):
         return e["strength"]
 
 def setStrength(e, strength):
         e["strength"] = strength
-        return
 
 def getResistance(e):
         return e["resistance"]
 
 def setResistance(e, resistance):
         e["resistance"] = resistance
-        return
 
 def getSprite(e):
        return e["sprite"]
 
 def setSprite(e, sprite):
        e["sprite"] = sprite
-       return
 
 if __name__ == "__main__":
        e = create(2, 2, 300, 0, 1, 1, "Test")
--- modules/Mob.py
+++ modules/Mob.py
@@ -10,85 +10,132 @@
 ##################################################################
 
 # Modules système
-
+import random
 # Modules personnalisés
 import Utils
-import Entity
 
 def create():
         mob = dict()
+        mob["x"]=3
+        mob["y"]=2
+        mob["health"]=100
+        mob["xp"]=0.0
+        mob["strength"]=1
+        mob["resistance"]=1
+        mob["sprite"]="M"
         mob["type"] = ""
         mob["state"] = "normal"
         mob["dir"] = (0, 0)
-        mob["entity"] = Entity.create(-1, -1, -1, -1, -1, -1, "")
         mob["damage"] = -1
         return mob
 
-def live(m, dt):
-        x, y = Entity.getPosition(m["entity"])
+#def setDirection(m):
+        #direction = [(0,-1),(0,1),(-1,0)(1,0)]
+        #m["dir"] = direction[random.randint(0,3)]
 
-        if m["type"] == "zombie":
-                m["dir"] = (-m["dir"](0), m["dir"](1))
-        elif m["type"] == "soldier":
-                m["dir"] = (-m["dir"](0), m["dir"](1))
-        elif m["type"] == "boss":
-                m["dir"] = (
-                x = 
-                y = m["dir"] = 
+#def getDirection(m):
+        #return mob["dir"]
 
-        Entity.setPosition(m["entity"], x, y)
-
-        return
-
-def show(m):
-        Entity.show(m)
-        return
-
-def getType(m):
-        return m["type"]
+#def setState(m):
+        #state = ["normal", "angry", "freeze"]
+        #currentState = getState
         
-def setType(m, type):
-        m["type"] = type
-        return
+        
 
-def getPosition(m):
-        return Entity.getPosition(m["entity"])
+#def getState(m):
+        #return m["state"]
+
+#def live(m):
+        #x, y = Entity.getPosition(m["entity"])
+
+        ##if mob["type"] == "zombie":
+                ##if mob["state"] == "normal":    
+                        
+                        ##Entity.collide()
+                        
+                        
+                ##elif mob["state"] == "angry":
+                        
+                        
+                ##elif mob["state"] == "freeze":  
+
+                        
+                
+        ##elif mob["type"] == "soldier":
+                ##if mob["state"] == "normal":
+                ##elif mob["state"] == "angry":
+                ##elif mob["state"] == "freeze": 
+                
+        ##elif mob["type"] == "boss":
+                ##if mob["state"] == "normal":
+                ##elif mob["state"] == "angry":
+                ##elif mob["state"] == "freeze": 
+        
+        
+        ###if m["type"] == "zombie":
+                ###m["dir"] = (-m["dir"](0), m["dir"](1))
+        ###elif m["type"] == "soldier":
+                ###m["dir"] = (-m["dir"](0), m["dir"](1))
+        ###elif m["type"] == "boss":
+                ###m["dir"] = (
+                ###x = 
+                ###y = m["dir"] = 
+
+        #Entity.setPosition(m["entity"], x, y)
+
+        #return
+
+
+
+#def show(m):
+        #Entity.show(m)
+        #return
+
+#def getType(m):
+        #return m["type"]
+        
+#def setType(m, type):
+        #m["type"] = type
+        #return
+
+#def getPosition(m):
+        #return Entity.getPosition(m["entity"])
      
-def setPosition(m, x, y):
-        Entity.setPosition(m["entity"], x, y)
-        return
+#def setPosition(m, x, y):
+        #Entity.setPosition(m["entity"], x, y)
+        #return
 
-def getHealth(m):
-        return Entity.getHealth(m["entity"]) 
+#def getHealth(m):
+        #return Entity.getHealth(m["entity"]) 
         
-def setHealth(m, health):
-        Entity.setHealth(m["entity"], health)
-        return
+#def setHealth(m, health):
+        #Entity.setHealth(m["entity"], health)
+        #return
 
-def getStrength(m):
-        return Entity.getStrength(m["entity"])
+#def getStrength(m):
+        #return Entity.getStrength(m["entity"])
         
-def setStrength(m, strength):
-        Entity.setStrength(m["entity"], strength)
-        return
+#def setStrength(m, strength):
+        #Entity.setStrength(m["entity"], strength)
+        #return
 
-def getResistance(m):              
-        return Entity.getResistance(m["entity"])
+#def getResistance(m):              
+        #return Entity.getResistance(m["entity"])
 
-def setResistance(m, resistance):
-        Entity.setResistance(m["entity"], resistance)
-        return
+#def setResistance(m, resistance):
+        #Entity.setResistance(m["entity"], resistance)
+        #return
 
-def getDamage(m):
-        return m["damage"]
+#def getDamage(m):
+        #return m["damage"]
         
-def setDamage(m, damage):
-        m["damage"] = damage
-        return
+#def setDamage(m, damage):
+        #m["damage"] = damage
+        #return
 
-def getSprite(m):
-        return Entity.getSprite(m["entity"])
+#def getSprite(m):
+        #return Entity.getSprite(m["entity"])
         
-def setSprite(m, sprite):
-        Entity.setSprite(m["entity"], sprite)
-        return
+#def setSprite(m, sprite):
+        #Entity.setSprite(m["entity"], sprite)
+        #return
