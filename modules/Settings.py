a9513da421c711ade8e66131ae7b6937850eafbc
dleveque <levequedo@gmail.com>
2016-06-01 18:37:04 +0200

    Ajout du module setting

---
+++ modules/Settings.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##################################################################
+##								##
+##	     The Legend Of Zelda - A Link to the Rogue		##
+##	    Un projet de Methode de Developpement (MDD) 	##
+##								##
+##			    Settings.py				##
+##								##
+## LEVEQUE Dorian & ROUE Evan 	S2P 	ENIB	     31/05/2016 ##
+##################################################################
+
+def create():
+        keyGame=dict()
+        keyGame["Up"] = 'z'
+        keyGame["Down"] = 's'
+        keyGame["Left"] = 'q'
+        keyGame["Right"] = 'd'
+        keyGame["Action"] = 'w'
+        return keyGame
+
+def getKey(keyGame,keyName):
+        for k in keyGame:
+                if k == keyName:
+                        return keyGame[k]
+
+def setKey(keyGame,keyName,key):
+        keyGame[keyName] = key
+        return
+        
+# Test
+if __name__ == "__main__":
+        gameSettings = create()
+
+        print getKey(gameSettings,'Up')
+        
+        setKey(gameSettings, 'Up', 'a')
+        
+        print getKey(gameSettings, 'Up')
