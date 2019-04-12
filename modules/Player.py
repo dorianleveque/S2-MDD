ffad5e007aa48317d03c8f6a37d33620e7e04cd7
Dorian44 <dorian.leveque@outlook.fr>
2019-04-12 17:53:05 +0200

    ajout des entetes

--- modules/Player.py
+++ modules/Player.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 ##################################################################
-##                                                                ##
-##             The Legend Of Zelda - A Link to the Rogue                ##
-##            Un projet de Methode de Developpement (MDD)         ##
-##                                                                ##
-##                             Player.py                                ##
-##                                                                ##
-## LEVEQUE Dorian & ROUE Evan         S2P         ENIB             09/04/2016 ##
+##                                                              ##
+##           The Legend Of Zelda - A Link to the Rogue          ##
+##          Un projet de Methode de Developpement (MDD)         ##
+##                                                              ##
+##                            Player.py                         ##
+##                                                              ##
+## LEVEQUE Dorian & ROUE Evan   S2P     ENIB         09/04/2016 ##
 ##################################################################
 
 # Modules systèmes
